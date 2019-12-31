f16a621b9bac783fc02d3b33b53200a458256be6
laura <laura.carrera.e@estudiant.upc.edu>
2019-12-31 01:15:44 +0100

    linear regression

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import numpy as np
 import preprocess
 import data_exploration
+import regression
 
 if __name__ == '__main__':
     data_df = pd.read_csv("../data/AB_NYC_2019.csv")
@@ -17,11 +18,14 @@ if __name__ == '__main__':
     preprocess.preprocess_dataset(data_df, to_numerical='le', norm_technique='z-score',
                                   exclude_norm_cols=['number_of_reviews', 'last_review', 'reviews_per_month'])
 
+    #print(data_df.isnull().values.any())
+    #print(data_df.columns)
+    #data = data_df.to_numpy()
+    data_df_y = data_df['price']
+    data_df_x = data_df.drop(columns=['price'])
+    data_x = data_df_x.to_numpy()
+    data_y = data_df_y.to_numpy()
 
-
-
-    print(data_df.isnull().values.any())
-    data = data_df.to_numpy()
-
-    # train_test_split()
+    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size = 0.33, random_state = 42)
+    pc = regression.linear_regression(x_train, y_train, x_test, y_test)
     # print(data_df.head())
--- src/regression.py
+++ src/regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score, confusion_matrix
+from scipy.stats import pearsonr
 
 
 
@@ -9,9 +9,8 @@ def linear_regression(x_train, y_train, x_test, y_test):
     reg = LinearRegression().fit(x_train, y_train)
     # Predict using x_test
     y_pred = reg.predict(x_test)
-    # Accuracy
-    a = accuracy_score(y_test, y_pred)
-    # Confusion Matrix
-    cm = confusion_matrix(y_test, y_pred)
-    return a, cm
+    pc = pearsonr(y_test, y_pred)
+    return pc
+
+
 
