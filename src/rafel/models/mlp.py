2e1517dd270bcb5b8daf301e6047a4673afbdbd4
dhilazo <david.hilazo.2015@salleurl.edu>
2020-01-02 14:15:40 +0100

    First MLP

--- src/rafel/models/mlp.py
+++ src/rafel/models/mlp.py
@@ -1,11 +1,25 @@
 import torch.nn as nn
 
 
-class MLP (nn.Module):
-    def __init__(self):
+class MLP(nn.Module):
+    def __init__(self, n_input_features):
         super().__init__()
-        self.hidden1 = nn.Linear(10, 10, True)
+        self.hidden1 = nn.Linear(n_input_features, 100)
+        self.hidden2 = nn.Linear(100, 50)
+        self.hidden3 = nn.Linear(50, 20)
+        self.out = nn.Linear(20, 1)
+
+        # Activations
+        self.leaky_relu = nn.LeakyReLU()
+        self.tanh = nn.Tanh()
 
     def forward(self, inp):
         x = self.hidden1(inp)
+        x = self.leaky_relu(x)
+        x = self.hidden2(x)
+        x = self.leaky_relu(x)
+        x = self.hidden3(x)
+        x = self.leaky_relu(x)
+        x = self.out(x)
+        x = self.tanh(x)
         return x
