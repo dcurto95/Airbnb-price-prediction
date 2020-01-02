fef49e7541b35ca0d0613058a59238990ad122a1
David Curto <dcurto95@gmail.com>
2020-01-02 18:14:11 +0100

    Add MSE and R2 metric in Linear Regression

--- src/regression.py
+++ src/regression.py
@@ -1,6 +1,8 @@
 from scipy.stats import pearsonr
 from sklearn.linear_model import LinearRegression
-
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import  numpy as np
 
 def linear_regression(x_train, y_train, x_test, y_test):
     # Train the model
@@ -8,4 +10,8 @@ def linear_regression(x_train, y_train, x_test, y_test):
     # Predict using x_test
     y_pred = reg.predict(x_test)
     pc = pearsonr(y_test, y_pred)
+    error = mean_squared_error(y_test, y_pred)
+    print('Mean squared error: %.2f' % error)
+    print('Coefficient of determination: %.2f'
+          % r2_score(y_test, y_pred))
     return pc
