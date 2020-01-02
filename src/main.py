7dfa673c1ddc0ba6df390a59e68e2a74689dc2a9
dhilazo <david.hilazo.2015@salleurl.edu>
2020-01-02 16:09:29 +0100

    Fixed bug

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     data_df = preprocess.clean_dataframe(data_df)
     data_df.to_csv(r'../data/AB_NYC_2019_cleaned.csv')
 
-    preprocess.preprocess_dataset(data_df, norm_technique='z-score',
+    data_df = preprocess.preprocess_dataset(data_df, norm_technique='z-score',
                                   exclude_norm_cols=['number_of_reviews', 'last_review', 'reviews_per_month'])
 
     # data_exploration.show_missing_data(data_df)
@@ -33,4 +33,5 @@ if __name__ == '__main__':
 
     x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.33, random_state=42)
     pc = regression.linear_regression(x_train, y_train, x_test, y_test)
+    print("\nRegression Pearson Correlation:", pc)
     # print(data_df.head())
