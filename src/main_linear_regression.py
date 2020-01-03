d90fe8b66e17663d27b0a0958c6ca118d53c909d
dhilazo <david.hilazo.2015@salleurl.edu>
2020-01-03 12:55:57 +0100

    Linear regression improved

--- src/main_linear_regression.py
+++ src/main_linear_regression.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
 import data_exploration
@@ -11,14 +12,15 @@ if __name__ == '__main__':
 
     data_exploration.show_missing_data(data_df)
     data_df = data_df.drop(columns=data_df.columns[:4])
+    data_df = data_df.drop(columns=['neighbourhood'])
     # data_exploration.show_data_exploration(data_df)
     data_df['last_review'] = pd.to_datetime(data_df['last_review'])
 
-    data_df = preprocess.clean_dataframe(data_df)
-    data_df.to_csv(r'../data/AB_NYC_2019_cleaned.csv')
+    # data_df = preprocess.clean_dataframe(data_df)
+    # data_df.to_csv(r'../data/AB_NYC_2019_cleaned.csv')
 
-    data_df = preprocess.preprocess_dataset(data_df, norm_technique='z-score',
-                                  exclude_norm_cols=['number_of_reviews', 'last_review', 'reviews_per_month'])
+    data_df = preprocess.preprocess_dataset(data_df, norm_technique='z-score')# ,
+                                  #exclude_norm_cols=['number_of_reviews', 'last_review', 'reviews_per_month'])
 
     # data_exploration.show_missing_data(data_df)
     # data_exploration.plot_correlation(data_df)
@@ -31,7 +33,8 @@ if __name__ == '__main__':
     data_x = data_df_x.to_numpy()
     data_y = data_df_y.to_numpy()
 
+    # data_x = PCA().fit_transform(data_x)
+
     x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.33, random_state=42)
-    pc = regression.linear_regression(x_train, y_train, x_test, y_test)
-    print("\nRegression Pearson Correlation:", pc)
+    regression.linear_regression(x_train, y_train, x_test, y_test)
     # print(data_df.head())
--- src/regression.py
+++ src/regression.py
@@ -1,17 +1,14 @@
 from scipy.stats import pearsonr
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
-import  numpy as np
+
 
 def linear_regression(x_train, y_train, x_test, y_test):
     # Train the model
     reg = LinearRegression().fit(x_train, y_train)
     # Predict using x_test
     y_pred = reg.predict(x_test)
-    pc = pearsonr(y_test, y_pred)
     error = mean_squared_error(y_test, y_pred)
     print('Mean squared error: %.2f' % error)
     print('Coefficient of determination: %.2f'
           % r2_score(y_test, y_pred))
-    return pc
