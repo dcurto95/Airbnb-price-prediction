5ca1f881f2709ac4529b47891a35aa7354152465
rafelps <rafel1995@gmail.com>
2020-01-06 21:48:18 +0100

    Adapt MLP to new dataset splits

--- src/dataloaders/airbnb.py
+++ src/dataloaders/airbnb.py
@@ -4,8 +4,6 @@ import os
 import numpy as np
 import pandas as pd
 
-import preprocess
-
 
 class AIRBNB(Dataset):
 
@@ -16,50 +14,15 @@ class AIRBNB(Dataset):
 
         data_df = pd.read_csv(os.path.join(path, data_set))
 
-        data_df.loc[(data_df['neighbourhood_group'] == 'Manhattan'), 'neighbourhood_group'] = data_df.loc[
-            (data_df['neighbourhood_group'] == 'Manhattan'), 'neighbourhood']
-        data_df = data_df.drop(columns=['neighbourhood'])
-        wrong_price_ids = np.where(data_df['price'] == 0)[0]
-        data_df = data_df.drop(wrong_price_ids)
-
-        # Logs
-        data_df['minimum_nights'] = np.log((data_df['minimum_nights']))
-        data_df['number_of_reviews'] = np.log(data_df['number_of_reviews'] + 1)
-        data_df['last_review'] = np.log(data_df['last_review'] + 1)
-        data_df['reviews_per_month'] = np.log(data_df['reviews_per_month'] + 1)
-        data_df['calculated_host_listings_count'] = np.log(data_df['calculated_host_listings_count'] + 1)
-        data_df['availability_365'] = np.log(data_df['availability_365'] + 1)
-
-        data_df, new_feature_cols = preprocess.preprocess_dataset(data_df, norm_technique='z-score')
-
-        data_df_y = data_df['price']
-        data_df_x = data_df.drop(columns=['price'])
-        data_x = data_df_x.to_numpy()
-        data_y = data_df_y.to_numpy()
-
-        data_y = np.log(data_y)
-
-        neighbourhood_cols = new_feature_cols[0]
-
-        neighbourhood_indexs = [list(data_df_x.columns).index(name) for name in neighbourhood_cols]
-        for neighbourhood_ind in neighbourhood_indexs:
-            x_train, y_train = preprocess.removal_of_price_outliers(data_x, data_y, data_x[:, neighbourhood_ind])
-
-
-        # data_df = preprocess.preprocess_dataset(data_df, norm_technique='z-score')
-        # self.data = data_df.to_numpy()
-        self.features = x_train
-        self.gt = y_train
-        # self.n_features = self.data.shape[1] - 1
+        self.targets = data_df['price'].to_numpy()
+        self.features = data_df.drop(columns=['price']).to_numpy()
         self.n_features = self.features.shape[1]
 
     def __len__(self):
-        # return self.data.shape[0]
-        return self.gt.shape[0]
+        return self.targets.shape[0]
 
     def __getitem__(self, index):
-        # return torch.FloatTensor(self.data[index, :-1].astype(np.float32)), torch.FloatTensor([self.data[index, -1].astype(np.float32)])
-        return torch.FloatTensor(self.features[index].astype(np.float32)), torch.FloatTensor([self.gt[index].astype(np.float32)])
+        return torch.FloatTensor(self.features[index].astype(np.float32)), torch.FloatTensor([self.targets[index].astype(np.float32)])
 
     def get_n_features(self):
         return self.n_features
