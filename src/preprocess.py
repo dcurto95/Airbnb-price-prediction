497c1920a4d9bac07a9a4eb15b12f6007e8c4f85
rafelps <rafel1995@gmail.com>
2020-01-06 20:19:50 +0100

    Refactor preprocess.py, rafel_preproc.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,10 @@
-import math
-import time
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
 
 
-def z_score_all_num_features(df, exclude_norm_cols=[]):
+def z_score_all_num_features(df, exclude_norm_cols=()):
     num_atts = df.select_dtypes(include=np.number).columns.to_list()
     for num_att in num_atts:
         if num_att not in exclude_norm_cols:
@@ -17,7 +15,7 @@ def z_score_normalization(array):
     return ((array - array.mean()) / array.std()).astype(np.float)
 
 
-def minmax_all_num_features(df, exclude_norm_cols=[]):
+def minmax_all_num_features(df, exclude_norm_cols=()):
     num_atts = df.select_dtypes(include=np.number).columns.to_list()
     for num_att in num_atts:
         if num_att not in exclude_norm_cols:
@@ -28,9 +26,8 @@ def minmax(array):
     return ((array - array.min()) / (array.max() - array.min())).astype(np.float)
 
 
-def one_hot_all_cat_features(df, avoid_class=False):
+def one_hot_all_cat_features(df):
     cat_atts = df.select_dtypes(exclude=np.number).columns.to_list()
-    cat_atts = cat_atts if not avoid_class or np.issubdtype(df.dtypes[-1], np.number) else cat_atts[:-1]
 
     new_encoded_attributes = []
     for cat_att in cat_atts:
@@ -79,69 +76,29 @@ def label_encoding(df, att):
     return {att + '_enc': sub_map}
 
 
-def fix_missing_values_from_dataset(dataset):
-    column_types = dataset.dtypes
-
-    for column_name, col_type in zip(dataset, column_types):
-        if np.issubdtype(col_type, np.number):
-            # Check if column is full of Missing values
-            if np.isnan(dataset[column_name]).all():
-                dataset.drop(columns=[column_name], inplace=True)
-            else:
-                mean = dataset[column_name].mean()
-                for value, index in zip(dataset[column_name], dataset.index):
-                    # Fix missing values
-                    if math.isnan(value):
-                        dataset.at[index, column_name] = mean
-        else:
-            # dataset[column_name] = dataset[column_name].astype(str)
-            values, count = np.unique(dataset[column_name], return_counts=True)
-            most_common_value = values[np.argmax(count)]
-
-            # If most common value is a missing value, get the next most common
-            if most_common_value == b'?':
-                max_sorted_indexes = np.argsort(count)[::-1]
-                for index in max_sorted_indexes:
-                    if values[index] != b'?':
-                        most_common_value = values[index]
-                        break
-
-            for value, index in zip(dataset[column_name], dataset.index):
-                # Fix missing values
-                if value == b'?':
-                    dataset.at[index, column_name] = most_common_value
-
-
-def preprocess_dataset(dataset, class_col, norm_technique="minmax", exclude_norm_cols=[]):
-    cols = dataset.columns.tolist()
-    cols.insert(len(cols), cols.pop(cols.index(class_col)))
-    dataset = dataset.reindex(columns=cols)
-
-    exclude_norm_cols.append(class_col)
+def preprocess_dataset(dataset, norm_technique="minmax", exclude_norm_cols=()):
     if norm_technique == "z-score":
         z_score_all_num_features(dataset, exclude_norm_cols=exclude_norm_cols)
     else:
         minmax_all_num_features(dataset, exclude_norm_cols=exclude_norm_cols)
 
-    new_features = one_hot_all_cat_features(dataset, avoid_class=True)
+    one_hot_all_cat_features(dataset)
+    # label_encoding_all_cat_features(dataset)
 
-    return dataset, new_features
-
-
-def clean_dataframe(dataset):
-    zero_reviews_index = np.where(dataset['number_of_reviews'] == 0)[0]
-    dataset.loc[(dataset['number_of_reviews'] == 0), 'reviews_per_month'] = 0
-    dataset.loc[(dataset['number_of_reviews'] == 0), 'last_review'] = np.min(dataset['last_review'])
-
-    date_to_timestamp(dataset)
     return dataset
 
 
-def preprocess_for_plot(dataset):
-    fix_missing_values_from_dataset(dataset)
-    z_score_all_num_features(dataset)
-    label_encoding_all_cat_features(dataset)
-    return dataset
+def clean_dataframe(df):
+    df.drop(columns=df.columns[:4], inplace=True)
+
+    df.drop(df[df['price'] == 0].index, inplace=True)
+
+    df.loc[(df['number_of_reviews'] == 0), 'reviews_per_month'] = 0
+
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    df.loc[(df['number_of_reviews'] == 0), 'last_review'] = np.min(df['last_review'])
+
+    return df
 
 
 def date_to_timestamp(df):
@@ -150,20 +107,17 @@ def date_to_timestamp(df):
     for date_att in date_atts:
         last_review = datetime.timestamp(np.max(df[date_att]))
 
-        mean = np.mean(df[date_att])
-        ind = np.where(pd.isnull(df[date_att]))
-        df.loc[ind[0], date_att] = mean
         df[date_att] = df[date_att].transform(lambda x: (last_review - datetime.timestamp(x)) / (24 * 3600))
+
     return df
 
 
-def removal_of_outliers(df, nhood, distance):
-    new_piece = df[df["neighbourhood_group"] == nhood]["log_price"]
+def removal_of_outliers(df, filter_by, filter_value, target, distance):
+    filtered_data = df[df[filter_by] == filter_value][target]
     # defining quartiles and interquartile range
-    q1 = new_piece.quantile(0.25)
-    q3 = new_piece.quantile(0.75)
+    q1 = filtered_data.quantile(0.25)
+    q3 = filtered_data.quantile(0.75)
     IQR = q3 - q1
 
-    trimmed = df[(df["neighbourhood_group"] == nhood) & (df.log_price > (q1 - distance * IQR)) & (
-                df.log_price < (q3 + distance * IQR))]
-    return trimmed
+    cleaned = df[(df[filter_by] == filter_value) & (df[target] > (q1 - distance * IQR)) & (df[target] < (q3 + distance * IQR))]
+    return cleaned
--- src/rafel_preproc.py
+++ src/rafel_preproc.py
@@ -9,68 +9,76 @@ from sklearn.neural_network import MLPRegressor
 import preprocess
 
 data_df = pd.read_csv('../data/AB_NYC_2019.csv')
-print(data_df.shape)
+print("Original shape", data_df.shape)
 
-data_df = data_df.drop(columns=data_df.columns[:4])
-data_df.loc[(data_df['neighbourhood_group'] == 'Manhattan'), 'neighbourhood_group'] = data_df.loc[
-    (data_df['neighbourhood_group'] == 'Manhattan'), 'neighbourhood']
-data_df = data_df.drop(columns=['neighbourhood'])
-wrong_price_ids = np.where(data_df['price'] == 0)[0]
-data_df = data_df.drop(wrong_price_ids)
-data_df['last_review'] = pd.to_datetime(data_df['last_review'])
+
+# Cleaning
 data_df = preprocess.clean_dataframe(data_df)
-# data_df.to_csv(r'../data/AB_NYC_2019_cleaned.csv', index=False)
 
-logged_cols = ['minimum_nights', 'number_of_reviews', 'reviews_per_month', 'calculated_host_listings_count',
-                'availability_365']
+
+# Transformations
+data_df = preprocess.date_to_timestamp(data_df)
+
+logged_cols = ['minimum_nights', 'number_of_reviews', 'reviews_per_month', 'last_review', 'calculated_host_listings_count', 'availability_365']
 for col in logged_cols:
-    data_df["log_(" + col + " +1)"] = np.log(data_df[col] + 1)
-data_df['log_price'] = np.log(data_df.price)
-data_df.drop(columns=logged_cols + ['price'], inplace=True)
+    data_df[col] = np.log(data_df[col] + 1)
+data_df['price'] = np.log(data_df.price)
 
-print(data_df.shape)
+print("Shape after cleaning and transformations", data_df.shape)
 
+
+# Cleaning outliers
 neighbourhoods = data_df.neighbourhood_group.unique().tolist()
 
 df_without_outliers = pd.DataFrame()
 for neighborhood in neighbourhoods:
-    a = preprocess.removal_of_outliers(data_df, neighborhood, 1.5)
-    df_without_outliers = df_without_outliers.append(a)
+    cleaned = preprocess.removal_of_outliers(data_df, 'neighbourhood_group', neighborhood, 'price', 1.5)
+    df_without_outliers = df_without_outliers.append(cleaned)
 
 data_df = df_without_outliers
 
-data_df, new_features = preprocess.preprocess_dataset(data_df, 'log_price', norm_technique='z-score')
 
-target = data_df['log_price'].to_numpy()
+# Change neighbourhood_group and neighbourhood
+# data_df.loc[(data_df['neighbourhood_group'] == 'Manhattan'), 'neighbourhood_group'] = data_df.loc[(data_df['neighbourhood_group'] == 'Manhattan'), 'neighbourhood']
+# data_df = data_df.drop(columns=['neighbourhood'])
 
-features = data_df.drop(['log_price'], axis=1).to_numpy()
 
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.33, random_state=1)
+# Normalize numerical and One-Hot categorical
+data_df = preprocess.preprocess_dataset(data_df, norm_technique='z-score', exclude_norm_cols='price')
 
-print("\nLinear regression:")
 
-reg = LinearRegression().fit(X_train, y_train)
-y_pred = reg.predict(X_test)
+# Separate into X, y
+target = data_df['price'].to_numpy()
+features = data_df.drop(columns=['price']).to_numpy()
 
-plt.scatter(y_pred, y_test)
-
-y_pred[np.where(y_pred < 0)[0]] = 0
-mse_reg = mean_squared_error(np.exp(y_test), np.exp(y_pred))
-r2_reg = r2_score(y_test, y_pred)
-
-print('\tMean squared error linear regression: %.2f' % mse_reg)
-print('\tCoefficient of determination linear regression: %.2f' % r2_reg)
-
-print("\nMulti Layer Perceptron:")
-mlp = MLPRegressor(activation='relu', max_iter=1000)
-mlp.fit(X_train, y_train)
-predictions = mlp.predict(X_test)
-predictions[np.where(predictions < 0)[0]] = 0
-
-mse_mlp = mean_squared_error(np.exp(y_test), np.exp(predictions))
-r2_mlp = r2_score(y_test, predictions)
-
-print('\tMean squared error linear regression: %.2f' % mse_mlp)
-print('\tCoefficient of determination linear regression: %.2f' % r2_mlp)
-
-plt.show()
+# X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3)
+#
+#
+# # Prediction methods
+# print("\nLinear regression:")
+#
+# reg = LinearRegression().fit(X_train, y_train)
+# y_pred = reg.predict(X_test)
+#
+# plt.scatter(y_pred, y_test)
+#
+# y_pred[np.where(y_pred < 0)[0]] = 0
+# mse_reg = mean_squared_error(np.exp(y_test), np.exp(y_pred))
+# r2_reg = r2_score(y_test, y_pred)
+#
+# print('\tMean squared error linear regression: %.2f' % mse_reg)
+# print('\tCoefficient of determination linear regression: %.2f' % r2_reg)
+#
+# print("\nMulti Layer Perceptron:")
+# mlp = MLPRegressor(activation='relu', max_iter=1000)
+# mlp.fit(X_train, y_train)
+# predictions = mlp.predict(X_test)
+# predictions[np.where(predictions < 0)[0]] = 0
+#
+# mse_mlp = mean_squared_error(np.exp(y_test), np.exp(predictions))
+# r2_mlp = r2_score(y_test, predictions)
+#
+# print('\tMean squared error linear regression: %.2f' % mse_mlp)
+# print('\tCoefficient of determination linear regression: %.2f' % r2_mlp)
+#
+# plt.show()
